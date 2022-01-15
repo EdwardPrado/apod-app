55b6a228047275a5e1380f65ead8f820e3b05cfd
Edward <edward.a.prado@gmail.com>
2022-01-15 16:56:56 -0500

    Added card thumbnail component.

--- src/pages/Likes/Likes.js
+++ src/pages/Likes/Likes.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./likes.scss";
 
 import Navbar from "../../components/Navbar/Navbar";
-import Card from "../../components/card/Card";
+import CardThumbnail from "../../components/cardThumbnail/CardThumbnail";
 
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
@@ -19,12 +19,13 @@ const Likes = () => {
 
 			for (let i = 0; i < storage.length; i++) {
 				cardArr.push(
-					<Card
+					<CardThumbnail
 						title={storage[i].title}
 						date={storage[i].date}
 						explanation={storage[i].explanation}
 						mediaType={storage[i].mediaType}
 						mediaURL={storage[i].mediaURL}
+						thumbnailURL={storage[i].thumbnail_url}
 					/>
 				);
 			}
@@ -42,25 +43,27 @@ const Likes = () => {
 	return (
 		<div>
 			<Navbar />
-			<div className="card-container">
-				{isLoading ? (
-					<Box
-						sx={{
-							top: 0,
-							left: 0,
-							bottom: 0,
-							right: 0,
-							position: "absolute",
-							display: "flex",
-							alignItems: "center",
-							justifyContent: "center",
-						}}
-					>
-						<div className="loading_backdrop"></div>
-						<CircularProgress />
-					</Box>
-				) : null}
-				{cards}
+			<div className="card-thumbnail_container">
+				<div className="card-thumbnail_grid">
+					{isLoading ? (
+						<Box
+							sx={{
+								top: 0,
+								left: 0,
+								bottom: 0,
+								right: 0,
+								position: "absolute",
+								display: "flex",
+								alignItems: "center",
+								justifyContent: "center",
+							}}
+						>
+							<div className="loading_backdrop"></div>
+							<CircularProgress />
+						</Box>
+					) : null}
+					{cards}
+				</div>
 			</div>
 		</div>
 	);
